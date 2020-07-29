aa54a2fd78712406e11e1042152ff9fee1cd8678
Graham Hughes <graehu@gmail.com>
2020-07-29 08:38:28 +0100

    Better formatting functions
    Makes it simpler to format text like so:
    
    log.normal(log.format.error("this bit's bad!")+" here's how you fix it.")

--- confply/log.py
+++ confply/log.py
@@ -1,24 +1,25 @@
 import os
 import confply.config
+from confply.config import confply_log_file
 
 # Logging module to keep logs consistent in confply.
 # todo: add formatted() for a formatted log function
 
 class format:
-    HEADER = lambda: '\033[95m' if confply.config.confply_log_file == None else ""
-    OKBLUE = lambda: '\033[94m' if confply.config.confply_log_file == None else ""
-    OKGREEN = lambda: '\033[92m' if confply.config.confply_log_file == None else ""
-    WARNING = lambda: '\033[93m' if confply.config.confply_log_file == None else ""
-    FAIL = lambda: '\033[91m' if confply.config.confply_log_file == None else ""
-    ENDC = lambda: '\033[0m' if confply.config.confply_log_file == None else ""
-    BOLD = lambda: '\033[1m' if confply.config.confply_log_file == None else ""
-    UNDERLINE = lambda: '\033[4m' if confply.config.confply_log_file == None else ""
+    header = lambda x: '\033[95m'+x+'\033[0m' if confply_log_file == None else x
+    ok_blue = lambda x: '\033[94m'+x+'\033[0m' if confply_log_file == None else x
+    ok_green = lambda x: '\033[92m'+x+'\033[0m' if confply_log_file == None else x
+    warning = lambda x: '\033[93m'+x+'\033[0m' if confply_log_file == None else x
+    error = lambda x: '\033[91m'+x+'\033[0m' if confply_log_file == None else x
+    bold = lambda x: '\033[1m'+x+'\033[0m' if confply_log_file == None else x
+    underline = lambda: '\033[4m'+x+'\033[0m' if confply_log_file == None else x
 
 def get_log_topic():
     return "[" + confply.config.confply_log_topic + "] "
 break_char = '='
 break_str = ""
 space_str = ""
+
 for x in range(0, 256): break_str += break_char
 for x in range(0, 256): space_str += " "
 
@@ -30,11 +31,11 @@ def linebreak():
 
 def bold(in_string):
     topic = get_log_topic()
-    print(topic+format.BOLD()+in_string+format.ENDC())
+    print(topic+format.bold(in_string))
 
 def underline(in_string):
     topic = get_log_topic()
-    print(topic+format.UNDERLINE()+in_string+format.ENDC())
+    print(topic+format.UNDERLINE(in_string))
 
 def header(in_string):
     topic = get_log_topic()
@@ -42,7 +43,7 @@ def header(in_string):
     in_string = " "+in_string+" "
     # magic number -2 makes logs look better in terminal editors
     # todo: break this line down
-    print(topic+break_str[int(len(in_string)/2+len(topic)):half_size]+format.HEADER()+in_string+format.ENDC()+break_str[:half_size-2-int(len(in_string)/2)])
+    print(topic+break_str[int(len(in_string)/2+len(topic)):half_size]+format.header(in_string)+break_str[:half_size-2-int(len(in_string)/2)])
     
 def centered(in_string):
     topic = get_log_topic()
@@ -59,12 +60,12 @@ def normal(in_string):
 
 def error(in_string):
     topic = get_log_topic()
-    print(topic+format.FAIL()+in_string+format.ENDC())
+    print(topic+format.error(in_string))
 
 def warning(in_string):
     topic = get_log_topic()
-    print(topic+format.WARNING()+in_string+format.ENDC())
+    print(topic+format.warning(in_string))
 
 def success(in_string):
     topic = get_log_topic()
-    print(topic+format.OKGREEN()+in_string+format.ENDC())
+    print(topic+format.ok_green(in_string))
