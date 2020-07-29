68fcfd2d4189d74a22e63a5cb8bcc385596d8954
Graham Hughes <graehu@gmail.com>
2020-07-29 22:27:40 +0100

    Tidied up log.py a bit.
    Just made it easier to read what's going on and a bit more stable in some ways.

--- confply/log.py
+++ confply/log.py
@@ -1,71 +1,67 @@
 import os
+import re
 import confply.config
-from confply.config import confply_log_file
 
 # Logging module to keep logs consistent in confply.
 # todo: add formatted() for a formatted log function
 
-class format:
-    header = lambda x: '\033[95m'+x+'\033[0m' if confply_log_file == None else x
-    ok_blue = lambda x: '\033[94m'+x+'\033[0m' if confply_log_file == None else x
-    ok_green = lambda x: '\033[92m'+x+'\033[0m' if confply_log_file == None else x
-    warning = lambda x: '\033[93m'+x+'\033[0m' if confply_log_file == None else x
-    error = lambda x: '\033[91m'+x+'\033[0m' if confply_log_file == None else x
-    bold = lambda x: '\033[1m'+x+'\033[0m' if confply_log_file == None else x
-    underline = lambda: '\033[4m'+x+'\033[0m' if confply_log_file == None else x
+def can_format():
+    return confply.config.confply_log_file == None
 
+class format:
+    header = lambda x: '\033[95m'+x+'\033[0m' if can_format() else x
+    ok_blue = lambda x: '\033[94m'+x+'\033[0m' if can_format() else x
+    ok_green = lambda x: '\033[92m'+x+'\033[0m' if can_format() else x
+    warning = lambda x: '\033[93m'+x+'\033[0m' if can_format() else x
+    error = lambda x: '\033[91m'+x+'\033[0m' if can_format() else x
+    bold = lambda x: '\033[1m'+x+'\033[0m' if can_format() else x
+    underline = lambda x: '\033[4m'+x+'\033[0m' if can_format() else x
+    
 def get_log_topic():
     return "[" + confply.config.confply_log_topic + "] "
-break_char = '='
-break_str = ""
-space_str = ""
 
-for x in range(0, 256): break_str += break_char
-for x in range(0, 256): space_str += " "
-
-def linebreak():
+def linebreak(break_char = "="):
     topic = get_log_topic()
-    # magic number -2 makes logs look better in terminal editors
-    terminal_size = os.get_terminal_size().columns-2
-    print(topic+break_str[len(topic):terminal_size])
-
+    # magic number -1 makes logs look better in terminal editors
+    terminal_size = os.get_terminal_size().columns - len(topic) -1
+    print(topic+''.center(terminal_size, break_char ))
+    
 def bold(in_string):
     topic = get_log_topic()
     print(topic+format.bold(in_string))
-
+    
 def underline(in_string):
     topic = get_log_topic()
-    print(topic+format.UNDERLINE(in_string))
-
-def header(in_string):
-    topic = get_log_topic()
-    half_size = int(((os.get_terminal_size().columns)/2))
-    in_string = " "+in_string+" "
-    # magic number -2 makes logs look better in terminal editors
-    # todo: break this line down
-    print(topic+break_str[int(len(in_string)/2+len(topic)):half_size]+format.header(in_string)+break_str[:half_size-2-int(len(in_string)/2)])
+    print(topic+format.underline(in_string))
     
-def centered(in_string):
+def header(in_string, break_char = "="):
+    in_string = format.underline(" "+in_string+" ")
+    centered(in_string, break_char)
+    
+def escape_ansi(line):
+    ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', line)
+    
+def centered(in_string, fill_string = " "):
     topic = get_log_topic()
-    half_size = int(((os.get_terminal_size().columns)/2))
-    in_string = " "+in_string+" "
-    # magic number -2 makes logs look better in terminal editors
-    # todo: break this line down
-    print(topic+space_str[int(len(in_string)/2+len(topic)):half_size]+in_string+space_str[:half_size-2-int(len(in_string)/2)])
-
+    ansi_delta = len(in_string) - len(escape_ansi(in_string))
+    # magic number -1 makes logs look better in terminal editors
+    terminal_width = os.get_terminal_size().columns - len(topic) - 1 + ansi_delta
+    in_string = (" "+in_string+" ").center(terminal_width, fill_string)
+    print(topic+in_string)
     
 def normal(in_string):
     topic = get_log_topic()
     print(topic+in_string)
-
+    
 def error(in_string):
     topic = get_log_topic()
     print(topic+format.error(in_string))
-
+    
 def warning(in_string):
     topic = get_log_topic()
     print(topic+format.warning(in_string))
-
+    
 def success(in_string):
     topic = get_log_topic()
     print(topic+format.ok_green(in_string))
