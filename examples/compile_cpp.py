371781c6e59f4add964971a18b531c563b047679
Graham Hughes <graehu@gmail.com>
2020-08-03 23:08:10 +0100

    Updated readme
    Doesn't really explain everything very well, but it's an iterative process

--- examples/compile_cpp.py
+++ examples/compile_cpp.py
@@ -1,12 +1,23 @@
-#!../confply.py test.py
+#!../confply.py
+# generated using:
+# python ../confply.py --config cpp_compiler compile_cpp.py
 from confply.cpp_compiler.config import *
+def log(in_str):
+    print("[cpp_compiler conf] "+in_str)
+log("confply_args: "+str(confply_args))
+
+debug = False
+if "debug" in confply_args:
+    debug = True
+    log("running debug")
+
 
 confply_tool = "clang"
 source_files = ["main.cpp"]
 output_file = "hello_confply"
 link_libraries = ["stdc++"]
-debug_info = True
-cpp_standard = "c++17"
+debug_info = debug
+standard = "c++17"
 warnings = ["all"]
 confply_log_config = True
 confply_log_topic = output_file
