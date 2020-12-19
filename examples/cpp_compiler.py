ce44085c05524a4cb2bfe896474648389c0046a8
Graham Hughes <graehu@gmail.com>
2020-12-19 17:35:24 +0000

    more cpp_compiler example tests
    * adds -tool to cpp_compiler example
    * adds -clean to cpp_compiler example
    * adds clang++, clang, g++, and gcc as Linux Example steps

--- examples/cpp_compiler.py
+++ examples/cpp_compiler.py
@@ -2,11 +2,12 @@
 # generated using:
 # python ../confply.py --config cpp_compiler compile_cpp.py
 import sys
+import os
 sys.path.append('..')
 import confply.cpp_compiler.config as confply
 import confply.log as log
-confply_log_topic = "compile_cpp.py"
-log.normal("loading compile_cpp.py with confply_args: "+str(confply.confply_args))
+confply.confply_log_topic = "cpp_compiler"
+log.normal("loading cpp_compiler with confply_args: "+str(confply.confply_args))
 
 debug = False
 if "debug" in confply.confply_args:
@@ -14,8 +15,20 @@ if "debug" in confply.confply_args:
     confply.object_path = "objects/debug"
     log.normal("set to debug config")
 
-
+# default tool is clang++
 confply.confply_tool = "clang++"
+
+if "-clean" in confply.confply_args:
+    log.normal("cleaning compiled objects")
+    os.system("rm -r objects/")
+    
+if "-tool" in confply.confply_args:
+    tool_index = confply.confply_args.index("-tool") + 1
+    if tool_index < len(confply.confply_args):
+        confply.confply_tool = confply.confply_args[tool_index]
+        log.normal("setting tool to "+confply.confply_tool)
+
+
 confply.source_files = ["main.cpp"]
 confply.output_file = "hello_confply"
 confply.link_libraries = ["stdc++"]
