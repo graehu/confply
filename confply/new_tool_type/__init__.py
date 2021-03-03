4e8baf241c37bd5ccae035de42060ebd5693c58a
Graham Hughes <graehu@gmail.com>
2021-03-03 06:55:04 +0000

    minor formatting
    trying to make python linters happy.

--- confply/new_tool_type/__init__.py
+++ confply/new_tool_type/__init__.py
@@ -6,20 +6,22 @@ import shutil
 
 tool = None
 
+
 # return the final command string or an array of commands
 def generate():
     return str(tool)+" no tool specific implementation"
+
 
 # return a dictionary of environment variables
 def get_environ():
     return os.environ
 
+
 # return if tool is found or not. (i.e. installed and on path)
 def is_found(in_tool=None):
-    if in_tool == None:
-        if config.confply.tool != None:
+    if in_tool is None:
+        if config.confply.tool is not None:
             return not shutil.which(config.confply.tool) is None
     else:
         return not shutil.which(in_tool) is None
     return False
-
--- confply/new_tool_type/echo.py
+++ confply/new_tool_type/echo.py
@@ -2,6 +2,7 @@ import confply.{tool_type} as {tool_type}
 
 tool_name = {tool_type}.os.path.basename(__file__)[:-3]
 
+
 def generate():
     {tool_type}.tool = tool_name
     return {tool_type}.generate()
